39b45b1070f9e76dc8263e1c598cc721cbcefe45
MdTasib <ohidulalam442@gmail.com>
2022-09-20 16:57:30 +0600

    get tours

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -7,7 +7,41 @@ const {
 // GET ALL TOOLS
 const getTools = async (req, res, next) => {
 	try {
-		const tools = await getToolsService();
+		let filters = { ...req.query };
+		const excludeFields = ["sort", "page", "limit"];
+		excludeFields.forEach(field => delete filters[field]);
+
+		let filtersString = JSON.stringify(filters);
+		filtersString = filtersString.replace(
+			/\b(gt|gte|lt|lte)\b/g,
+			match => `$${match}`
+		);
+		filters = JSON.parse(filtersString);
+
+		// QUERIES
+		const queries = {};
+
+		if (req.query.sort) {
+			const sortBy = req.query.sort.split(",").join(" ");
+			queries.sortBy = sortBy;
+		}
+
+		if (req.query.fields) {
+			const fields = req.query.fields.split(",").join(" ");
+			queries.fields = fields;
+		}
+
+		if (req.query.page || req.query.limit) {
+			const { page = 1, limit = 10 } = req.query;
+
+			const skip = (page - 1) * Number(limit);
+			queries.skip = skip;
+			queries.limit = Number(limit);
+		}
+
+		console.log("queries", queries);
+
+		const tools = await getToolsService(filters, queries);
 
 		res.status(200).json({
 			status: "Success",
--- services/tool.services.js
+++ services/tool.services.js
@@ -1,8 +1,18 @@
 const Tool = require("../models/index");
 
-const getToolsService = async () => {
-	const tools = await Tool.find({});
-	return tools;
+const getToolsService = async (filters, queries) => {
+	const { skip, limit = 10, fields, sortBy } = queries;
+
+	const tools = await Tool.find(filters)
+		.skip(skip)
+		.limit(limit)
+		.select(fields)
+		.sort(sortBy);
+
+	const totalTools = await Tool.countDocuments(filters);
+	const pageCount = Math.ceil(totalTools / limit);
+
+	return { totalTools, pageCount, tools };
 };
 
 const getToolDetailService = async id => await Tool.find({ _id: id });
